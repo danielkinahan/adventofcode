9389e4ea3744d0ca1a04ed06fab5223a134ea862
Daniel Kinahan <danielkinahan@uvica.ca>
2023-12-02 15:08:25 -0500

    4th star completed

--- 2/main.py
+++ 2/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 input = "2/input.txt"
 
 totalCubes = {
@@ -8,17 +10,37 @@ totalCubes = {
 
 def isPossibleGame(values):
     for draw in values.split("; "):
-        for cubes in draw.split(", "):
-            count, colour = cubes.split(" ")
+        for cube in draw.split(", "):
+            count, colour = cube.split(" ")
             if int(count) > totalCubes[colour.strip()]:
                 return False
-    return True 
+    return True
+
+def minimumSetPower(values):
+    maxCubes = {
+        "red": 0,
+        "green": 0,
+        "blue": 0
+    }
+    for draw in values.split("; "):
+        for cube in draw.split(", "):
+
+            count, colour = cube.split(" ")
+            colour = colour.strip()
+            count = int(count)
+            if count > maxCubes[colour]:
+                maxCubes[colour] = count
+
+    return np.product(list(maxCubes.values()))
 
 ids = []
+powers = []
 with open(input, 'r') as file:
     for line in file:
         game, values = line.split(": ")
         id = int(game.replace("Game ", ""))
         if isPossibleGame(values):
             ids.append(id)
+        powers.append(minimumSetPower(values))
 print(sum(ids))
+print(sum(powers))
